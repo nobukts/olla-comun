fa8ba42fd5b00e5a7724734d7fd5533d676c18ae
José Osega <jjcoroneljj@gmail.com>
2023-05-29 17:50:23 -0400

    Intento de navbar
    No hay más, intento de navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <header>
+      <ul>
+        <li><a href="#"> Hola </a> </li>
+        <li><a href="#"> Buenas </a> </li>
+        <li><a href="#"> GOOOODDDD </a> </li>
+        <li><a href="#"> Dios </a> </li>
+        <li><a href="#"> Epico </a> </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Navbar
