ae1a53b16e02b9881019c198a46450c3dfbe2ae3
José Osega <jjcoroneljj@gmail.com>
2023-05-31 18:34:04 -0400

    Actualización barra de navegador

--- src/components/BarraNavegacion/BarraNavegacion.jsx
+++ src/components/BarraNavegacion/BarraNavegacion.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './BarraNavegacion.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,17 +8,20 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function BarraNavegacion() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand>Olla Comun</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Container >
+        <Navbar.Brand>
+          <img className='logo' src="/src/assets/Logo página.png"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#inicio">Inicio</Nav.Link>
             <Nav.Link href="#buscar-lugares">Buscar Lugares</Nav.Link>
             <Nav.Link href="#añadir-olla">Añadir Olla Común</Nav.Link>
           </Nav>
-          <Nav>
-            <NavDropdown title="Cuenta" id="collasible-nav-dropdown">
+          <Nav className='cuenta-usuario'>
+            <img className='usuario' src="/src/assets/Logo Usuario.png"/>
+            <NavDropdown className='opc-cuenta' title="Cuenta" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#cuenta/crear-cuenta">Crear Cuenta</NavDropdown.Item>
               <NavDropdown.Item href="#cuenta/iniciar-sesion">Iniciar Sesión</NavDropdown.Item>
               <NavDropdown.Divider />
