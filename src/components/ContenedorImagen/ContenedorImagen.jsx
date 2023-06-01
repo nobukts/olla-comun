1344c272e86c8c13022893148e343b3957e10816
José Osega <jjcoroneljj@gmail.com>
2023-06-01 15:35:47 -0400

    Prueba de copiar estilo inicial del html

---
+++ src/components/ContenedorImagen/ContenedorImagen.jsx
@@ -0,0 +1,19 @@
+//CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+import './ContenedorImagen.css'
+
+//LOGO
+import example_photo from '../../assets/olla-comun-example.jpg'
+
+export default function ContenedorImagen(){
+    return (
+
+        <div className="ex-photo">
+        <figure className="oc-img">
+            <img src={example_photo}/>
+            <figcaption>Olla comun realizada durante el año 2020</figcaption>
+        </figure>
+
+      </div>
+    );
+}
