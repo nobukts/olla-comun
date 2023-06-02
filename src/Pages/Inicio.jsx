363aab0972146506e319a7871adb25feecde81a5
José Osega <jjcoroneljj@gmail.com>
2023-06-02 15:33:30 -0400

    Actualización visual

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,36 +1,37 @@
+import { Col, Row, Accordion } from "react-bootstrap";
 import ContenedorImagen from "../components/ContenedorImagen/ContenedorImagen";
-import Accordion from "react-bootstrap/Accordion";
+
+import './Inicio.css'
 
 function Inicio() {
   return (
-    <div>
-      <ContenedorImagen />
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+    <div >
+      <Row style={{display: "flex", alignItems: "center",justifyContent: "center", height: "100%"}}>
+        <Col > <ContenedorImagen/> </Col>
+        <Col>
+          <h1>Información general</h1>
+          <Accordion defaultActiveKey="0" className="bloq-info">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>1. ¿Qué es una olla común?</Accordion.Header>
+            <Accordion.Body>
+              Una olla común es una actividad solidaria que ha sido utilizada desde hace varias decadás para batir uno de los problemas más comunes en las sociedades actuales, el hambre. <br/> A través de donaciones solidarias entre vecinos o el apoyo de alguna ONG, se realiza una organización para realizar platos de comida y tratar de ayudar a la gente sin casa o con problemas financieros.<br/> En el caso de nuestro país, Chile, se han realizado desde el año 1929 ollas comunes segun informa la fundación Gente de la calle, y durante la pandemia se realizaron diversas instancias debido a que realzó la desigualdad existente en el país.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>2. ¿Quiénes somos?</Accordion.Header>
+            <Accordion.Body>
+              Somos un grupo de estudiantes, cuyo objetivo es apoyar a las ONG y personas comúnes para darle mayor visibilidad a las ollas comunes, debido a que hemos podido observar la necesidad de estas actividades en nuestra sociedad post-pandemia.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>3. Información de contacto</Accordion.Header>
+            <Accordion.Body>
+              Bryan López: bryan.lopez.a@mail.pucv.cl
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </Col>
+      </Row>
     </div>
   );
 }
