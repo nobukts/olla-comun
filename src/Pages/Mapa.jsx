b8a1eb370886f56d9fec57e2d8737aeba99c51ef
José Osega <jjcoroneljj@gmail.com>
2023-06-03 21:58:41 -0400

    Actualización Mapa

--- src/Pages/Mapa.jsx
+++ src/Pages/Mapa.jsx
@@ -1,38 +1,43 @@
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
-
+import { Link } from "react-router-dom";
 import "./Mapa.css"
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Mapa() {
-    const { isLoaded } = useLoadScript({ googleMapsApiKey: "AIzaSyB00PBkxvd20TcPcypy7-b9VVqmHYW-Im0" })
-    
-    var latitud = -32.927252
-    var longitud = -71.520808
-    var z = 15
-
-    function obtenerDireccion(){
-        latitud = -35
-        longitud = -72
-        z = 10
-    }
-
-    if (!isLoaded) return <div>Loading...</div>
-
-    return <div className="mapa"> 
-        <Row>
-            <h1 className="ti-mapa">Mapa</h1>
-        </Row>
-        <Row>
-            <Col>
-                <h2>Listado de ollas comunes</h2>
-                <button onClick={obtenerDireccion}>
-                    Seleccionar casa del bryan    
-                </button>            
-            </Col>
-            <Col>
-                <GoogleMap zoom={z} center={{ lat: latitud, lng: longitud }} mapContainerClassName="map-container">
-                </GoogleMap>
-            </Col>
-        </Row>
-    </div>
+    return(
+        <Container fluid>
+            <div className="barra">
+                <Row className="barra-titular">
+                    <Col xs={11}>
+                        <h1>Listado de direcciones en formato mapa</h1> 
+                    </Col>
+                    <Col>
+                        <Link to="/BuscarLugares"> Volver atrás</Link>
+                    </Col>
+                </Row>
+            </div>
+            <Row>
+                <Col className="col-lateral">
+                    <h2 className="mini-titu">Listado de ollas comunes</h2>
+                    <ul className="direcciones">
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                        <li><button>Hola</button></li>
+                    </ul>
+                </Col>
+                <Col xs="9">
+                    <iframe className="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d215143.74574820994!2d-71.65068324753919!3d-32.589629527251816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9689b0516498c055%3A0x32e0680d98a733d9!2sZapallar%2C%20Valpara%C3%ADso!5e0!3m2!1ses-419!2scl!4v1685842087714!5m2!1ses-419!2scl"></iframe>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
